7e41ecaa9072483defaaa3ca6c68c3441fb1bc17
Drew <hilarioandrew12@gmail.com>
2025-04-26 14:29:29 +0800

    Remove unused prettify-min.js file from static files

--- grocery_pos_backend/settings.py
+++ grocery_pos_backend/settings.py
@@ -189,8 +189,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # For development (optional)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
