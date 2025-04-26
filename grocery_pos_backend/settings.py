4c21f4f360dd857acefaad5a45270b663821d963
Drew <hilarioandrew12@gmail.com>
2025-04-26 14:24:58 +0800

    Update static file settings for improved handling and organization

--- grocery_pos_backend/settings.py
+++ grocery_pos_backend/settings.py
@@ -185,11 +185,12 @@ SPECTACULAR_SETTINGS = {
 USE_DEPRECATED_PYTZ = False
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # For development (optional)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
