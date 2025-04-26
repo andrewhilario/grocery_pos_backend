f51085bbe218aa06626da812c231e02044005524
Drew <hilarioandrew12@gmail.com>
2025-04-26 13:57:34 +0800

    Update ALLOWED_HOSTS to allow all hosts during development

--- grocery_pos_backend/settings.py
+++ grocery_pos_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-x96#$a0+tlcm_@4j-lp%o1ys%8ro)mnzf2!wfsl7()6jsffgsm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
